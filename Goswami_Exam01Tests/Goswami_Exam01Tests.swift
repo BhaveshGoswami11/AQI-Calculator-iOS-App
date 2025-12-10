7f77238563931f42decf4700e30ac47cb09c64d4
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-12-10 08:12:06 -0600

    Create Goswami_Exam01Tests.swift

---
+++ Goswami_Exam01Tests/Goswami_Exam01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Goswami_Exam01Tests.swift
+//  Goswami_Exam01Tests
+//
+//  Created by Bhavesh on 10/7/25.
+//
+
+import Testing
+@testable import Goswami_Exam01
+
+struct Goswami_Exam01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
